4729067d4d224d6e037a97955023ab3065a7810f
jh <>
2024-12-13 00:24:26 -0500

    got a mvp working for dns explorer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,96 @@
 import { useState } from "react";
 import "./App.css";
 import { D3Widget } from "@d3-inc/marketplace-widget";
+import { DNSConnect } from "@webinterop/dns-connect";
+import { Button, Form, Columns, Container } from "react-bulma-components";
+
+const { Input, Field, Control, Label, Select } = Form;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [chain, setChain] = useState("CORE");
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+
   const walletConnectKey = "";
+  const dnsConnect = new DNSConnect();
+
+  async function resolve() {
+    // Resolves `example.core` name on `CORE` blockchain
+    const walletAddress = await dnsConnect.resolve(input, chain);
+    if (walletAddress) {
+      setResult(walletAddress);
+    }
+    console.log(walletAddress);
+  }
+
+  async function reverseResolve() {
+    // Reverse resolves wallet address `0xaaaa` on `CORE` blockchain
+    const domainName = await dnsConnect.reverseResolve(input, chain);
+    if (domainName) {
+      setResult(domainName);
+    }
+    console.log(domainName);
+  }
 
   return (
     <>
+      <Columns className="is-centered">
+        <h1 className="title is-1">D3 DNS Explorer</h1>
+      </Columns>
+
+      <Container className="is-max-tablet padding-top">
+        <Columns>
+          <Field className="column ">
+            <Label>Domain to resolve</Label>
+            <Control>
+              <Input
+                placeholder="test.core"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </Control>
+          </Field>
+
+          <Field className="column is-one-quarter">
+            <Label>Select a chain</Label>
+            <Control>
+              <Select
+                onChange={function updateChain(value) {
+                  console.log(value.currentTarget.value);
+                  setChain(value.currentTarget.value);
+                }}
+                value={chain}
+              >
+                <option value="ETH">ETH</option>
+                <option value="BTC">BTC</option>
+                <option value="BONE">BONE</option>
+                <option value="CORE">CORE</option>
+                <option value="VIC">VIC</option>
+                <option value="MATIC">MATIC</option>
+                <option value="ADA">ADA</option>
+                <option value="APE">APE</option>
+              </Select>
+            </Control>
+          </Field>
+        </Columns>
+      </Container>
+
+      <Container className="is-max-tablet padding-top">
+        <Columns className="is-centered">
+          <Field className="column is-one-quarter">
+            <Button onClick={resolve}>Resolve</Button>
+          </Field>
+          <Field className="column is-one-quarter">
+            <Button onClick={reverseResolve}>Reverse Resolve</Button>
+          </Field>
+        </Columns>
+      </Container>
+
+      <Container className="is-max-tablet padding-top">
+        <div>Resolved result: {result}</div>
+      </Container>
+
       <D3Widget
         appName="Your app name"
         config={{
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import "@d3-inc/marketplace-widget/styles.css";
+import "bulma/css/bulma.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
